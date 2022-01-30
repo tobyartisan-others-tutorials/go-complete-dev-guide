eba2ea29e7af60c5d2ab6900c325b9c0a5e76a01
Toby Artisan <tobyartisan@outlook.com>
2022-01-29 17:32:32 -0700

    Removed commented code.

--- section-3/cards/deck.go
+++ section-3/cards/deck.go
@@ -61,12 +61,10 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
-	//rand.Seed(time.Now().UnixNano())
 	source := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(source)
 
 	for i := range d {
-		//newPosition := rand.Intn(len(d) - 1)
 		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
