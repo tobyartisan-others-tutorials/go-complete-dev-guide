4fd53eea9e8e623144340c9cc7c58c2a0f059925
Toby Artisan <tobyartisan@outlook.com>
2022-01-31 20:48:28 -0700

    Create a map and delete a value.

---
+++ map/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var colors map[string]string
+
+	colors := make(map[int]string)
+
+	// colors := map[string]string {
+	// 	"red": "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
+
+	fmt.Println(colors)
+}
