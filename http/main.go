56e37d455fe3f30f055cd764dca1951415883e8c
Toby Artisan <tobyartisan@outlook.com>
2022-02-05 14:43:07 -0700

    Create our own logWriter that implements the Writer interface to output web page.

--- http/main.go
+++ http/main.go
@@ -7,6 +7,14 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 
@@ -19,5 +27,7 @@ func main() {
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
 }
